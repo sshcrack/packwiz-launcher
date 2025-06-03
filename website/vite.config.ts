71a16d2c2d8a1726595d7f550723fe366fa9713b
sshcrack <34072808+sshcrack@users.noreply.github.com>
2025-06-03 17:55:55 +0200

    Support for SPA

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -7,10 +7,11 @@ export default defineConfig({
   plugins: [
     react(), tsconfigPaths()
   ],
-  optimizeDeps:{
-		exclude:["img-to-ico"]
-	},
+  optimizeDeps: {
+    exclude: ["img-to-ico"]
+  },
   server: {
     cors: true // Allow all origins for development
-  }
+  },
+  base: './' // Set base to relative path for GitHub Pages deployment
 });
